b41943aba7b7d87a7be06cb2bcc90a4160f55a79
jslusser <jslusser@gmail.com>
2017-07-18 14:55:47 -0400

    Recommit

---
+++ Project16/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by James Slusser on 6/16/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/16/overview
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
